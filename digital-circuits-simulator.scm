6138371f0961e4184e7e0bbf2197b8c40c834bf8
tiendo1011 <tiendo1011@gmail.com>
2020-12-20 09:39:55 +0700

    Wire together all components for digital circuits simulator

--- digital-circuits-simulator.scm
+++ digital-circuits-simulator.scm
@@ -1,9 +1,12 @@
+(load "queue.scm")
+
 (define (inverter input output)
   (define (invert-input)
     (let ((new-value (logical-not (get-signal input))))
          (after-delay inverter-delay
                       (lambda () (set-signal! output new-value)))))
   (add-action! input invert-input) 'ok)
+
 (define (logical-not s)
   (cond ((= s 0) 1)
         ((= s 1) 0)
@@ -20,6 +23,13 @@
   (add-action! a2 and-action-procedure)
   'ok)
 
+(define (logical-and s1 s2)
+  (cond ((and (= s1 1) (= s2 1)) 1)
+        ((and (= s1 1) (= s2 0)) 0)
+        ((and (= s1 0) (= s2 1)) 0)
+        ((and (= s1 0) (= s2 0)) 0)
+        (else (error "Invalid signal" s1 s2))))
+
 (define (or-gate a1 a2 output)
   (define (or-action-procedure)
     (let ((new-value
@@ -30,6 +40,13 @@
   (add-action! a1 or-action-procedure)
   (add-action! a2 or-action-procedure)
   'ok)
+
+(define (logical-or s1 s2)
+  (cond ((and (= s1 1) (= s2 1)) 1)
+        ((and (= s1 1) (= s2 0)) 1)
+        ((and (= s1 0) (= s2 1)) 1)
+        ((and (= s1 0) (= s2 0)) 0)
+        (else (error "Invalid signal" s1 s2))))
 
 (define (make-wire)
   (let ((signal-value 0) (action-procedures '()))
@@ -61,6 +78,21 @@
 (define (add-action! wire action-procedure)
   ((wire 'add-action!) action-procedure))
 
+(define (half-adder a b s c)
+  (let ((d (make-wire)) (e (make-wire)))
+       (or-gate a b d)
+       (and-gate a b c)
+       (inverter c e)
+       (and-gate d e s)
+       'ok))
+
+(define (full-adder a b c-in sum c-out)
+  (let ((s (make-wire)) (c1 (make-wire)) (c2 (make-wire)))
+       (half-adder b c-in s c1)
+       (half-adder a s sum c2)
+       (or-gate c1 c2 c-out)
+       'ok))
+
 (define (propagate)
   (if (empty-agenda? the-agenda)
       'done
@@ -79,7 +111,7 @@
                        (newline)
                        (display name) (display " ")
                        (display (current-time the-agenda))
-                       (display "New-value = ")
+                       (display " New-value = ")
                        (display (get-signal wire)))))
 
 (define (make-time-segment time queue)
@@ -138,3 +170,22 @@
            (set-current-time! agenda
                               (segment-time first-seg))
            (front-queue (segment-queue first-seg)))))
+
+(define the-agenda (make-agenda))
+(define inverter-delay 2)
+(define and-gate-delay 3)
+(define or-gate-delay 5)
+
+(define input-1 (make-wire))
+(define input-2 (make-wire))
+(define sum (make-wire))
+(define carry (make-wire))
+
+(probe 'sum sum)
+(probe 'carry carry)
+(half-adder input-1 input-2 sum carry)
+(set-signal! input-1 1)
+(propagate)
+
+(set-signal! input-2 1)
+(propagate)
